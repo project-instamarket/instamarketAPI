9aa2588727cbca65981fb2b8b11eb119324ec5cc
Bolaji Olajide <cooproton@gmail.com>
2019-05-01 16:59:34 +0100

    initial commit

---
+++ config/default.js
@@ -0,0 +1,28 @@
+import dotenv from 'dotenv';
+import { getEnvVar } from 'env-utils';
+
+
+dotenv.config();
+
+const lazyGetEnvVar = (...args) => () => getEnvVar(...args);
+
+module.exports = {
+  isDev: () => process.env.NODE_ENV === 'development',
+  isStaging: () => process.env.NODE_ENV === 'staging',
+  isProd: () => process.env.NODE_ENV === 'production',
+
+  app: {
+    port: lazyGetEnvVar('PORT', { devDefault: '8081', optional: true }),
+  },
+
+  authentication: {
+    jwtSecret: lazyGetEnvVar('JWT_SECRET', { devDefault: 'loochloosa' }),
+  },
+
+  db: {
+    host: lazyGetEnvVar('DB_HOST', { devDefault: '127.0.0.1' }),
+    name: lazyGetEnvVar('DB_NAME'),
+    password: lazyGetEnvVar('DB_PASSWORD'),
+    user: lazyGetEnvVar('DB_USER'),
+  }
+};
