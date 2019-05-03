ef3b1612fd2fa9f17bebaf79e07c587d95ffe259
Bolaji Olajide <cooproton@gmail.com>
2019-05-03 03:08:37 +0100

    add new line to files

--- knexfile.js
+++ knexfile.js
@@ -1,44 +1,17 @@
-// Update with your config settings.
+import config from 'lazy-config';
 
 module.exports = {
-
-  development: {
-    client: 'sqlite3',
-    connection: {
-      filename: './dev.sqlite3'
-    }
+  client: 'pg',
+  connection: {
+    database: 'my_db',
+    user: 'username',
+    password: 'password'
   },
-
-  staging: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
+  pool: {
+    min: 2,
+    max: 10
   },
-
-  production: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
+  migrations: {
+    tableName: 'knex_migrations'
   }
-
 };
